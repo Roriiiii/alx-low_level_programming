bcea4ae5f2eeb886dcec09dcb4838f829dd71606
root <merryhaileysus0961.com@gmail.com>
2023-02-16 03:11:40 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include < stdio.h > 
 /**
  * main - A function that displays the given input
- *Return: 0 
+ *Return: 0(success)  
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
